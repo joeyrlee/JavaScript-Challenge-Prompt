708e6ce1699775e235cf83c980d0a4d9f9da3824
joeyrlee <jleepianist@gmail.com>
2017-05-14 11:28:34 -0700

    (completed prompt) Finished prompt with es5 & es6 syntax

--- hrsf52-self-assessment-week-02-v3-master/js-inheritance-pseudo-classical-alt/js-inheritance-pseudo-classical-alt.js
+++ hrsf52-self-assessment-week-02-v3-master/js-inheritance-pseudo-classical-alt/js-inheritance-pseudo-classical-alt.js
@@ -26,11 +26,9 @@ var makeFlyingHorse = function(name, color) {
   result.color = color;
   var oldGoSomewhere = result.goSomewhere;
   result.goSomewhere = function(destination, milesToDestination) {
-    if (milesToDestination < 10) {
-      return oldGoSomewhere(destination);
-    } else {
-      return name + ' is flying to ' + destination + '!';
-    }
+    return milesToDestination < 10
+      ? oldGoSomewhere(destination)
+      : name + ' is flying to ' + destination + '!';
   };
   return result;
 };
@@ -38,12 +36,59 @@ var makeFlyingHorse = function(name, color) {
 // YOUR WORK GOES BELOW
 // Here's some starter code to get you going!
 
+//ES5 Solution
 var Horse = function(name) {
+  this.name = name;
 };
 
-
+Horse.prototype.goSomewhere = function(destination) {
+  return this.name + ' is galloping to ' + destination + '!';
+};
 
 var FlyingHorse = function(name, color) {
+  this.name = Horse.call(name);
+  this.color = color;
 };
+
+FlyingHorse.prototype = Object.create(Horse.prototype);
+FlyingHorse.constructor = FlyingHorse;
+
+FlyingHorse.prototype.oldGoSomewhere = Horse.prototype.goSomewhere;
+
+FlyingHorse.prototype.goSomewhere = function(destination, milesToDestination) {
+  return milesToDestination < 10
+    ? this.oldGoSomewhere(destination)
+    : return name + ' is flying to ' + destination + '!';
+}
+
+
+
+//ES6 Solution
+class Horse {
+  constructor(name) {
+    this.name = name;
+  }
+
+  goSomewhere(destination) {
+    return `${this.name} is galloping to ${destination}`;
+  }
+}
+
+class makeFlyingHorse extends Horse {
+  constructor(name, color) {
+    super(name);
+    this.color = color;
+  }
+
+  goSomewhere(destination) {
+    return milesToDestination < 10
+      ? this.oldGoSomewhere(destination)
+      : `${this.name} is flying to ${destination}`;
+  }
+
+  oldGoSomewhere() {
+    return super.goSomewhere(destination);
+  }
+}
 
 
